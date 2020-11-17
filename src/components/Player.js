b6d1b90a353d27bb5383be6c094dfc7120538066
Jakub Ziemba <jayziembka@gmail.com>
2020-11-18 00:35:32 +0100

    Create new components

---
+++ src/components/Player.js
@@ -0,0 +1,12 @@
+export default function Player() {
+  return (
+    <div className='player'>
+      <div className='time-control'>
+        <p>Start Time</p>
+        <input type='range' />
+        <p>End Time</p>
+      </div>
+      <div className='play-control'></div>
+    </div>
+  );
+}

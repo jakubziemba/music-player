f2e551bab9ffe663d155d6b9e817ee28f30fdb7c
Jakub Ziemba <jayziembka@gmail.com>
2020-11-18 01:08:48 +0100

    Add icons

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
+
 export default function Player() {
   return (
     <div className='player'>
@@ -6,7 +13,19 @@ export default function Player() {
         <input type='range' />
         <p>End Time</p>
       </div>
-      <div className='play-control'></div>
+      <div className='play-control'>
+        <FontAwesomeIcon
+          className='skip-back'
+          icon={faAngleLeft}
+          size='2x'
+        />
+        <FontAwesomeIcon className='play' icon={faPlay} size='2x' />
+        <FontAwesomeIcon
+          className='skip-forward'
+          icon={faAngleRight}
+          size='2x'
+        />
+      </div>
     </div>
   );
 }

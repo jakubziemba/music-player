8976ebcb2f3b99bb15e7811fc834a2e81ef0a2f7
Jakub Ziemba <jayziembka@gmail.com>
2021-03-04 20:17:22 +0100

    Update Song component

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,7 +1,7 @@
 export default function Song({ currentSong }) {
   return (
     <div className='song-container'>
-      <img src={currentSong.cover} alt='Song album cover'></img>
+      <img src={currentSong.cover} alt={`${currentSong.name} Album Cover`} />
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
